9d36d789639a06d832aba37e737d867b6600d1fa
Stephen Bussey <steve.bussey@salesloft.com>
2016-03-15 00:03:51 -0400

    initial

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+require('normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
