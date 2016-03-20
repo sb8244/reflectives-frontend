b92b6e9fccc86d56d5ebe5942e9f31037b221436
Stephen Bussey <steve.bussey@salesloft.com>
2016-03-19 21:31:40 -0400

    use dev tools middleware

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,7 +2,11 @@ const redux = require('redux');
 const reducers = require('../reducers');
 
 module.exports = function(initialState) {
-  const store = redux.createStore(reducers, initialState)
+  const store = redux.createStore(
+    reducers,
+    initialState,
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
