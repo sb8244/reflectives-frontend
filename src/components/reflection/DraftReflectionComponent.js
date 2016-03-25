1da5d1bcbc53a1916d10b1ec2f224d6cde6f80bf
Stephen Bussey <steve.bussey@salesloft.com>
2016-03-25 13:18:54 -0400

    replace quill with draft-js

---
+++ src/components/reflection/DraftReflectionComponent.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+
+require('styles/reflection/DraftReflection.scss');
+
+class DraftReflectionComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+    const {editorState} = this.state;
+
+    let className = 'RichEditor-editor';
+    var contentState = editorState.getCurrentContent();
+    if (!contentState.hasText()) {
+      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
+        className += ' RichEditor-hidePlaceholder';
+      }
+    }
+
+    return (
+      <div className={className}>
+        <Editor editorState={editorState}
+                     onChange={this.onChange}
+                     handleKeyCommand={this.handleKeyCommand.bind(this)}
+                     placeholder={`Reflect on ${this.props.theme}`} />
+      </div>
+    );
+  }
+}
+
+DraftReflectionComponent.displayName = 'ReflectionDraftReflectionComponent';
+export default DraftReflectionComponent;
--- src/components/reflection/ThemeReflectionComponent.js
+++ src/components/reflection/ThemeReflectionComponent.js
@@ -1,45 +1,22 @@
 'use strict';
 
-import React, { Component } from 'react';
-import ReactQuill from 'react-quill';
+import React from 'react';
+import DraftReflectionComponent from './DraftReflectionComponent';
 
-require('styles/reflection/ThemeReflection.scss');
-
-const STYLES = {
-  '.ql-editor': {
-    'font-family': '"Arial", san-serif',
-    'font-size': '18px'
-  },
-  '.ql-editor a': {
-    'text-decoration': 'none'
-  }
-};
-
-class ThemeReflectionComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { content: '' };
-  }
-
-  onTextChange(value) {
-    this.setState({ content: value });
-  }
-
-  render() {
-    return (
-      <div className='row align-center'>
-        <div className='medium-10 large-10 columns theme-reflection--wrapper'>
-          <ReactQuill value={ this.state.content } theme='snow' onChange={this.onTextChange.bind(this)} styles={STYLES} />
-        </div>
+let ThemeReflectionComponent = (props) => {
+  return (
+    <div className='row align-center'>
+      <div className='medium-10 large-10 columns theme-reflection--wrapper'>
+        <DraftReflectionComponent theme={getTheme(props)} />
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-// function getTheme(props) {
-//   let index = props.themeId || 0;
-//   return props.themes.get(index);
-// }
+function getTheme(props) {
+  let index = props.themeId || 0;
+  return props.themes.get(index);
+}
 
 ThemeReflectionComponent.displayName = 'ReflectionThemeReflectionComponent';
 
