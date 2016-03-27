1aeebc8f9a0752921698bd4c22a27b3e0db7822f
Stephen Bussey <steve.bussey@salesloft.com>
2016-03-27 16:23:08 -0400

    serialize state to local storage so it is persistent between refreshes
    I think that persistConfig will get pulled out into helper arrays if this gets more complex

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,12 +1,31 @@
 const redux = require('redux');
 const reducers = require('../reducers');
 import ReduxThunk from 'redux-thunk';
+import persistState from 'redux-localstorage';
+import Immutable from 'immutable';
+
+let persistConfig = {
+  serialize: function(collection) {
+    let serializableCollection = Object.assign({}, collection);
+    serializableCollection.themes = serializableCollection.themes.toJSON();
+    return JSON.stringify(serializableCollection);
+  },
+  deserialize: function(serializedData) {
+    let data = JSON.parse(serializedData);
+    data.themes.items.forEach(theme => {
+      delete theme.timerId;
+    });
+    data.themes = Immutable.fromJS(data.themes);
+    return data;
+  }
+};
 
 module.exports = function(initialState) {
   const store = redux.createStore(
     reducers,
     initialState,
     redux.compose(
+      persistState(null, persistConfig),
       redux.applyMiddleware(ReduxThunk),
       window.devToolsExtension ? window.devToolsExtension() : undefined
     )
