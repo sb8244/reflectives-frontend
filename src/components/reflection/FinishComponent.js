e8d042530530d57f7cb313507a3c9f66326b7071
Stephen Bussey <steve.bussey@salesloft.com>
2016-03-29 17:56:18 -0400

    style finish screen + date selection component

--- src/components/reflection/FinishComponent.js
+++ src/components/reflection/FinishComponent.js
@@ -1,21 +1,41 @@
 'use strict';
 
 import React from 'react';
+import moment from 'moment';
+import Immutable from 'immutable';
+import DateSelectionForm from '../DateSelectionForm';
 
 require('styles/reflection/Finish.scss');
 
 let FinishComponent = () => (
   <div className='row align-center'>
     <div className='medium-10 large-10 columns reflection-finish--wrapper'>
-      <div className="text-center">
-        <i className="fa fa-thumbs-o-up" />
-        <p className="lead">
-          Great Job! This reflection will be stashed away so that you can access it in the future.
+      <div className='text-center'>
+        <i className='fa fa-thumbs-o-up' />
+        <p className='lead'>
+          Great Job! This reflection has been stashed away so that you can access it in the future.
         </p>
+
+        <p>
+          You can have your reflection emailed to you within the next 7 days. This
+          can be useful to remind you of your thoughts and keep momentum with your goals.
+        </p>
+
+        <DateSelectionForm dates={next7Days()} />
       </div>
     </div>
   </div>
 );
+
+function next7Days() {
+  let dates = Immutable.Range(1, 8).map(days => moment().add(days, 'days'));
+  return dates.map(time => {
+    return {
+      time: time.unix(),
+      display: 'Next ' + time.format('dddd')
+    };
+  }).toArray();
+}
 
 FinishComponent.displayName = 'ReflectionFinishComponent';
 
