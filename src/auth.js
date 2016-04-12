ee78e2eb0db0167a4c341994611bc6d1c19d5a52
Stephen Bussey <steve.bussey@salesloft.com>
2016-04-12 01:02:46 -0400

    login / logout via passwordless

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,13 @@
 export const auth = {
   loggedIn: function() {
     return !!localStorage.token
+  },
+
+  setToken: function(token) {
+    localStorage.token = token;
+  },
+
+  logout: function() {
+    delete localStorage.token;
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,6 @@ import WiredFinishComponent from './containers/WiredFinishComponent';
 import WiredLoginComponent from './containers/WiredLoginComponent';
 import {auth} from './auth';
 
-function requireAuth(nextState, replace) {
-  if (!auth.loggedIn()) {
-    replace({
-      pathname: '/login',
-      state: { nextPathname: nextState.location.pathname }
-    })
-  }
-}
-
 export const routeConfig = [
   {
     path: '/',
@@ -20,8 +11,16 @@ export const routeConfig = [
     onEnter: requireAuth
   },
   {
+    path: '/auth',
+    onEnter: submitAuth
+  },
+  {
     path: '/login',
     component: WiredLoginComponent
+  },
+  {
+    path: '/logout',
+    onEnter: logout
   },
   {
     path: '/reflect/:themeId',
@@ -36,3 +35,48 @@ export const routeConfig = [
     component: WiredFinishComponent
   }
 ];
+
+function requireAuth(nextState, replace) {
+  if (!auth.loggedIn()) {
+    replace({
+      pathname: '/login',
+      state: { nextPathname: nextState.location.pathname }
+    })
+  }
+}
+
+function submitAuth(nextState, replace, callback) {
+  var token, uid;
+  ({ token, uid } = nextState.location.query);
+
+  let authPromise = new Promise((resolve, reject) => {
+    if (token && uid) {
+      let data = new FormData();
+      data.append('token', token);
+      data.append('uid', uid);
+      fetch(`http://localhost:8001/auth?token=${token}&uid=${uid}`).then(response => {
+        if (response.status === 200) {
+          response.json().then(json => {
+            auth.setToken(json.token);
+            resolve();
+          });
+        } else { reject(); }
+      }).catch(reject);
+    } else {
+      reject();
+    }
+  });
+
+  authPromise.then(() => {
+    replace('/');
+    callback();
+  }).catch(() => {
+    replace('/login');
+    callback();
+  });
+}
+
+function logout(nextState, replace) {
+  auth.logout();
+  replace('/login');
+}
